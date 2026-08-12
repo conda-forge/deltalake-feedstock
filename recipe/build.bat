b018a758f0560df1e9412e0bce44c5b68649866e
Michał Górny <mgorny@gentoo.org>
2026-08-12 21:12:15 +0200

    Bump to 1.6.2
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,8 +1,9 @@
+cd python || exit 1
+
 REM license file inclusion by pip fails with 'Failed to write to *.dist-info/licenses/licenses/deltalake_license.txt'
 REM since we are including it in the conda package separately, let's not do it with pip too
-sed -i.bak '/^license =/d' pyproject.toml
+sed -i.bak '/^license =/d' pyproject.toml || exit 1
 
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation || exit 1
 
-cd python
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,7 @@
+set -e -x
+
+cd python
+
 # Taken from clangdev's recipe
 # https://github.com/conda-forge/clangdev-feedstock/blob/01fc5e3e0fc690db85151dcb3ff512e6aa876be7/recipe/build.sh#L51-L56
 # disable -fno-plt due to some GCC bug causing linker errors, see
@@ -39,5 +43,4 @@ fi
 
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
 
-cd python
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
