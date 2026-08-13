ed64fe1487fb26223e770fef16d5e77674190685
Michał Górny <mgorny@gentoo.org>
2026-08-13 15:21:14 +0200

    Use shorter `CARGO_HOME` on Windows
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- recipe/build.bat
+++ recipe/build.bat
@@ -4,6 +4,8 @@ REM license file inclusion by pip fails with 'Failed to write to *.dist-info/lic
 REM since we are including it in the conda package separately, let's not do it with pip too
 sed -i.bak '/^license =/d' pyproject.toml || exit 1
 
+REM avoid path too long problem
+set CARGO_HOME=C:\
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation || exit 1
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
